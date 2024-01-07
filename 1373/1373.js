9a9139245941cc06f50a1565ca254d78a24eda1f
rulewrite <rulewrite@gmail.com>
2024-01-07 17:58:01 +0900

    fix: 경계값 대응

--- 1373/1373.js
+++ 1373/1373.js
@@ -10,7 +10,13 @@ const input = require('fs')
 let octal = '';
 
 for (let i = input.length; i >= 0; i -= 3) {
-  octal = parseInt(input.slice(Math.max(i - 3, 0), i), 2).toString(8) + octal;
+  const sliced = input.slice(Math.max(i - 3, 0), i);
+
+  if (!sliced) {
+    break;
+  }
+
+  octal = parseInt(sliced, 2).toString(8) + octal;
 }
 
 console.log(octal);
