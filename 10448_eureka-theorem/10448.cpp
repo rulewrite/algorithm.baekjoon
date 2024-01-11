9c9ff5a87d3556f5bf19c9723cf276f2a54298bf
rulewrite <rulewrite@gmail.com>
2024-01-11 23:10:21 +0900

    feat: 10448 유레카 이론 c++

---
+++ 10448_eureka-theorem/10448.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int getDotCount(int n) {
+  return n * (n + 1) / 2;
+}
+
+int main(void) {
+  // freopen("example.txt", "r", stdin);
+
+  int count;
+  scanf("%d", &count);
+
+  int triangles[count];
+  for (int i = 0; i < count; ++i) {
+    scanf("%d", &triangles[i]);
+  }
+
+  for (int i = 0; i < count; ++i) {
+    bool isFound = false;
+    int triangle = triangles[i];
+
+    int j = 1;
+    while (!isFound) {
+      int firstDotCount = getDotCount(j);
+
+      if (firstDotCount >= triangle) {
+        break;
+      }
+
+      int k = 1;
+      while (!isFound) {
+        int secondDotCount = getDotCount(k);
+
+        if (secondDotCount >= triangle) {
+          break;
+        }
+
+        int l = 1;
+        while (!isFound) {
+          int totalDotCount = firstDotCount + secondDotCount + getDotCount(l);
+          // printf("%d, %d, %d, = %d =? %d\n", j, k, l, totalDotCount, triangle);
+
+          if (totalDotCount == triangle) {
+            isFound = true;
+
+            break;
+          }
+
+          if (totalDotCount > triangle) {
+            break;
+          }
+
+          ++l;
+        }
+
+        ++k;
+      }
+
+      ++j;
+    }
+
+    printf("%d\n", isFound);
+  }
+
+  return 0;
+}
