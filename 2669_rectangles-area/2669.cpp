acbf40762d999a7676cba22ad6caacbf140f204d
rulewrite <rulewrite@gmail.com>
2024-01-26 11:41:22 +0900

    refactor: 입력, 연산 병합

--- 2669_rectangles-area/2669.cpp
+++ 2669_rectangles-area/2669.cpp
@@ -4,20 +4,13 @@ int main(void) {
   // freopen("example.txt", "r", stdin);
 
   int const RECTANGLE_COUNT = 4;
-  int rectangles[RECTANGLE_COUNT][4];
-
-  for (int i = 0; i < RECTANGLE_COUNT; ++i) {
-    scanf("%d %d %d %d", &rectangles[i][0], &rectangles[i][1], &rectangles[i][2], &rectangles[i][3]);
-    // printf("%d %d %d %d\n", rectangles[i][0], rectangles[i][1], rectangles[i][2], rectangles[i][3]);
-  }
-
   int const SIZE = 100;
   int area[SIZE][SIZE] = { 0 };
+
   for (int i = 0; i < RECTANGLE_COUNT; ++i) {
-    int leftX = rectangles[i][0];
-    int bottomY = rectangles[i][1];
-    int rightX = rectangles[i][2];
-    int topY = rectangles[i][3];
+    int leftX, bottomY, rightX, topY;
+
+    scanf("%d %d %d %d", &leftX, &bottomY, &rightX, &topY);
 
     for (int x = leftX; x < rightX; ++x) {
       for (int y = bottomY; y < topY; ++y) {
